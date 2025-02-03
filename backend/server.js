c2875dedde61eadeb4936cff37ae48d88b1f018c
KAZI AZAHAR UDDIN <kazirimo123@gmail.com>
2025-02-03 20:12:10 +0530

    Fix: Added ES module compatible __dirname

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 import aiRoutes from "./routes/aiRoutes.js";
 import { connectDB } from "./config/db.js"
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
